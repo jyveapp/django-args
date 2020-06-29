ab18034ececc22059002d65a3887315db743e1a9
Wes Kendall <wes.kendall@jyve.com>
2020-06-29 11:01:38 -0500

    Add a ``select_for_update`` option to ``djarg.qset``
    The ``select_for_update`` option for ``djarg.qset`` can be used to dynmaically
    apply ``select_for_update`` on the queryset whenever one is not running in
    a partial ``python-args`` mode (such as validation-only)
    
    Type: feature

--- djarg/tests/test_utils.py
+++ djarg/tests/test_utils.py
@@ -5,7 +5,59 @@ import ddf
 import django.contrib.auth.models as auth_models
 import pytest
 
-import djarg
+import djarg.utils
+
+
+def test_attach_select_for_update():
+    """
+    Verifies that _attached_select_for_update behaves as expected on
+    a queryset
+    """
+    qset = auth_models.User.objects.all()
+
+    # A select_for_update should never be attached to a function
+    # not running under python-args
+    new_qset = djarg.utils._attach_select_for_update(qset, None)
+    assert not new_qset.query.select_for_update
+
+    new_qset = djarg.utils._attach_select_for_update(qset, True)
+    assert not new_qset.query.select_for_update
+
+    # Test verious scenarios of properly adding a select_for_update
+    @arg.defaults(
+        qset=djarg.qset('qset', model=auth_models.User, select_for_update=True)
+    )
+    def no_args_select_for_update(qset):
+        return qset
+
+    assert no_args_select_for_update(qset).query.select_for_update
+
+    @arg.defaults(
+        qset=djarg.qset(
+            'qset', model=auth_models.User, select_for_update=['self']
+        )
+    )
+    def of_arg_select_for_update(qset):
+        return qset
+
+    assert of_arg_select_for_update(qset).query.select_for_update
+    assert of_arg_select_for_update(qset).query.select_for_update_of == [
+        'self'
+    ]
+
+    @arg.defaults(
+        qset=djarg.qset(
+            'qset',
+            model=auth_models.User,
+            select_for_update={'of': ['self'], 'skip_locked': True},
+        )
+    )
+    def kwarg_select_for_update(qset):
+        return qset
+
+    assert kwarg_select_for_update(qset).query.select_for_update
+    assert kwarg_select_for_update(qset).query.select_for_update_of == ['self']
+    assert kwarg_select_for_update(qset).query.select_for_update_skip_locked
 
 
 @pytest.mark.django_db
